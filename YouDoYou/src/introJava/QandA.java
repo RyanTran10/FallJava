4a82c9e7321e10a500fb6fe544dd7404c6981683
ryant <ryant@localhost>
2021-09-21 08:32:06 -0400

    upload

---
+++ YouDoYou/src/introJava/QandA.java
@@ -0,0 +1,33 @@
+package introJava;
+
+public class QandA {
+	
+	public static void main(String[] args) {
+		System.out.println("Where are you from?");
+		System.out.println("Southborough, Massachusetts (Right where St.Marks is).");
+		System.out.println("");
+		
+		System.out.println("What do you do outside of school?");
+		System.out.println("I like to play videogames outside of school (mainly valorant).");
+		System.out.println("");
+		
+		System.out.println("What is your favorite class?");
+		System.out.println("I don't have one favorite class but I really like all of my STEM classes.");
+		System.out.println("");
+		
+		System.out.println("Do you have any siblings?");
+		System.out.println("I have an older sister named Lauren who is a senior here.");
+		System.out.println("");
+		
+		System.out.println("What is your favorite show / videogame?");
+		System.out.println("I really like Criminal Minds and Valorant.");
+		System.out.println("");
+		
+		System.out.println("What is your favorite food?");
+		System.out.println("I do not have one particular favorite food but I really like sushi, ramen, and Korean bbq.");
+		
+	
+	}
+	
+
+}
---
+++ YouDoYou/src/introJava/Sep15Class.java
@@ -0,0 +1,13 @@
+package introJava;
+
+public class Sep15Class {
+	
+	public static void main(String[] args) {
+		
+		int myAge = 9;
+		System.out.println(myAge);
+		
+	}
+	
+
+}
