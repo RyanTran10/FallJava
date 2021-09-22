b4033760a5273506035d77ce9295a0862246cf2b
ryant <ryant@localhost>
2021-09-22 13:13:53 -0400

    HEYYYYY JUDY

--- Java I/src/introJava/OperationsWorksheet.java
+++ Java I/src/introJava/OperationsWorksheet.java
@@ -6,7 +6,7 @@ public class OperationsWorksheet {
 		System.out.println(x%10);
 	}
 	public void numToChar(int x) {
-		System.out.println((char)x);
+		System.out.print((char)x);
 	}
 	public void exactAverage(double x, double y) {
 		System.out.println((x+y)/2);
@@ -16,7 +16,9 @@ public class OperationsWorksheet {
 		
 		OperationsWorksheet runner = new OperationsWorksheet();
 		
-		runner.numToChar(1000);
+		for (Integer i = 0; i < 6000; i++ ) {
+			runner.numToChar(i);
+		}
 		runner.lastDigit(1423);
 		runner.exactAverage(24, 4214);
 		
