90d0a8c07aa7c853d63821ea72dc4be67091b710
ryant <ryant@localhost>
2021-09-24 11:47:50 -0400

    Hi Anika

---
+++ Java I/src/introJava/Clock.java
@@ -0,0 +1,35 @@
+package introJava;
+
+public class Clock {
+	private int time;
+	// create your methods here
+	public void setTime(int num) {
+		time = num;
+	}
+	public void tick() {
+		time = (time + 1)%60;
+	}
+	public void displayTime() {
+		System.out.println(time);
+	}
+	public static void main(String[] args) {
+		Clock tester = new Clock();
+		
+		// should display the time to be 56, 57, 58, 59, 0, 1, etc.
+		tester.setTime(56);
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+	}
+}
