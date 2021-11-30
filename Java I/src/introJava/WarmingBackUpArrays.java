f0d527bcc56eb76b90e186266510f0dd7ae50658
ryant <ryant@localhost>
2021-11-30 12:03:54 -0500

    Nov 30

--- Java I/src/introJava/WarmingBackUpArrays.java
+++ Java I/src/introJava/WarmingBackUpArrays.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 public class WarmingBackUpArrays {
 //	Write a method that takes a number n as parameter.
 //	You should then build an array containing the first n values of the Fibonacci sequence. Print this array.
+	public static void printEach(String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
 	public static void fibSequence(int n) {
 		int[] arr = new int[n];
 		arr[0] = 0;
@@ -17,6 +23,38 @@ public class WarmingBackUpArrays {
 		System.out.println(Arrays.toString(arr));
 	}
 	
+	public static void trim(int[] arr) {
+		int[] arr2 = new int[arr.length-2];
+		int min = arr[0];
+		int max = arr[0];
+		int error = 0;
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}else if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] != min && arr[i] != max) {
+				arr2[i-error] = arr[i];
+			}else {
+				error++;
+			}
+		}
+		System.out.println(Arrays.toString(arr2));
+	}
+	
+	public static void divAhead(int[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i]%arr[i-1] == 0) {
+				System.out.println("Yes");
+			}else {
+				System.out.println("No");
+			}
+		}
+	}
+	
 	public static void risingAverage(int[] arr) {
 		double[] arr2 = new double[arr.length];
 		double total = 0;
@@ -26,7 +64,25 @@ public class WarmingBackUpArrays {
 		}
 		System.out.println(Arrays.toString(arr2));
 	}
+	
+	public static void reverse(String[] arr, int n) {
+		for (int i = 0; i < n; i++) {
+			String temp = arr[i];
+			arr[i] = arr[arr.length-i-1];
+			arr[arr.length-i-1] = temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void dotProduct(int[] arr, int[] arr2) {
+		int total = 0;
+		for(int i = 0; i < arr.length; i++) {
+			total += arr[i] * arr2[i];
+		}
+		System.out.println(total);
+	}
+	
 	public static void main(String[] args) {
-		risingAverage(new int[] {4, 5, 2, 7});
+		printEach(new String[] {"I", "love", "C", "S"});
 	}
 }
