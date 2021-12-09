50b0c62e507f554cb28975715399a2f946491acc
ryant <ryant@localhost>
2021-12-09 09:34:26 -0500

    Dec 9

--- Java I/src/introJava/MoreWithStrings.java
+++ Java I/src/introJava/MoreWithStrings.java
@@ -25,7 +25,36 @@ public class MoreWithStrings {
 	public static void replaceChars(String s, String c) {
 		System.out.println(s.replaceAll(c, ""));
 	}
+	//1234
+	//1243
+	//1342
+	//1324
+	//1423
+	//1432
+	//2134
+	//2143
+	//2314
+	//2341
+	//2431
+	//2413
+	//3124
+	//3142
+	//3241
+	//3214
+	//3412
+	//3421
+	//4123
+	//4132
+	//4213
+	//4231
+	//4321
+	//4312
 	
+//	public static void anagram(String s) {
+//		for(int i = 0; i < s.length(); i++) {
+//			
+//		}
+//	}
 	public static void main(String[] args) {
 		replaceChars("let's eat there", "e");
 	}
