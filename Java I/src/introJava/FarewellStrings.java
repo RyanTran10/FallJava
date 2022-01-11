b1b07700b3c942a8331c0703fdf0558f43ca7d8c
ryant <ryant@localhost>
2022-01-11 08:52:29 -0500

    Jan 11

---
+++ Java I/src/introJava/FarewellStrings.java
@@ -0,0 +1,32 @@
+package introJava;
+
+import java.util.Arrays;
+
+public class FarewellStrings {
+	public static void pigLatin(String s) {
+		String[] words = s.split(" ");
+		for(int i = 0; i < words.length; i++) {
+			words[i] = words[i].substring(1) + words[i].charAt(0) + "ay";
+		}
+		System.out.println(Arrays.toString(words));
+	}
+	
+	public static void seperate(String s, char c) {
+		for(int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i) == c ? "$" : s.charAt(i));
+		}
+	}
+	
+	private static void longestWord(String[] words) {
+		String longest = "";
+		for(int i = 0; i < words.length; i++) {
+			if(words[i].length() > longest.length()) {
+				longest = words[i];
+			}
+		}
+		System.out.println(longest);
+	}
+	public static void main(String[] args) {
+		pigLatin("Hello my name is jeff. How are you today!");
+	}
+}
